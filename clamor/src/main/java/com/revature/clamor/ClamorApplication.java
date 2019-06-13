bc06c42189f323102a95e4e512d6b9683d9cb682
Michele Dexter <micheleannedexter@gmail.com>
2019-06-13 13:25:53 -0400

    server starter package

---
+++ clamor/src/main/java/com/revature/clamor/ClamorApplication.java
@@ -0,0 +1,13 @@
+package com.revature.clamor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClamorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClamorApplication.class, args);
+	}
+
+}
